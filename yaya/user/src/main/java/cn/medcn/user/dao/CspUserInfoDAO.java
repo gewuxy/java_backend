971aef67f79e87c32352872f4fdd91e5f048528b
刘长玲 <changling_liu@medcn.cn>
2017-09-26 19:31:46 +0800

    cspUserService

---
+++ yaya/user/src/main/java/cn/medcn/user/dao/CspUserInfoDAO.java
@@ -0,0 +1,10 @@
+package cn.medcn.user.dao;
+
+import cn.medcn.user.model.CspUserInfo;
+import com.github.abel533.mapper.Mapper;
+
+/**
+ * Created by Liuchangling on 2017/9/26.
+ */
+public interface CspUserInfoDAO extends Mapper<CspUserInfo> {
+}
---
+++ yaya/user/src/main/java/cn/medcn/user/service/CspUserService.java
@@ -0,0 +1,11 @@
+package cn.medcn.user.service;
+
+import cn.medcn.common.service.BaseService;
+import cn.medcn.user.model.CspUserInfo;
+
+/**
+ * Created by Liuchangling on 2017/9/26.
+ */
+public interface CspUserService extends BaseService<CspUserInfo>{
+
+}
