da2dc0ef31fde9d14d43e7a941e9e179b4190d81
liuliping <1047923274@qq.com>
2017-09-26 19:33:57 +0800

    新增

---
+++ yaya/user/src/main/java/cn/medcn/user/dao/CspUserInfoDAO.java
@@ -0,0 +1,12 @@
+package cn.medcn.user.dao;
+
+import cn.medcn.user.model.BindInfo;
+import cn.medcn.user.model.CspUserInfo;
+import com.github.abel533.mapper.Mapper;
+
+/**
+ * Created by LiuLP on 2017/4/20.
+ */
+public interface CspUserInfoDAO extends Mapper<CspUserInfo>{
+
+}
---
+++ yaya/user/src/main/java/cn/medcn/user/dao/UserFluxDAO.java
@@ -0,0 +1,11 @@
+package cn.medcn.user.dao;
+
+import cn.medcn.user.model.UserFlux;
+import com.github.abel533.mapper.Mapper;
+
+/**
+ * Created by LiuLP on 2017/4/20.
+ */
+public interface UserFluxDAO extends Mapper<UserFlux>{
+
+}
