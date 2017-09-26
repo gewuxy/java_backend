6ffeea2468cca0810ad1985750632d4e3497d1e3
lixuan <lixuan@medcn.cn>
2017-09-26 20:05:45 +0800

    修改国际化

---
+++ yaya/csp/src/main/java/cn/medcn/csp/interceptor/LocalInterceptor.java
@@ -0,0 +1,40 @@
+package cn.medcn.csp.interceptor;
+
+import cn.medcn.common.utils.CheckUtils;
+import cn.medcn.common.utils.CookieUtils;
+import cn.medcn.common.utils.LocalUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static cn.medcn.common.Constants.DEFAULT_LOCAL;
+
+/**
+ * Created by lixuan on 2017/9/26.
+ */
+public class LocalInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String local = httpServletRequest.getHeader("_local");
+        if (CheckUtils.isEmpty(local)) {
+            local = CookieUtils.getCookieValue(httpServletRequest, "_local");
+            if (CheckUtils.isEmpty(local)) {
+                local = DEFAULT_LOCAL;
+            }
+        }
+        LocalUtils.set(LocalUtils.getByKey(local));
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
