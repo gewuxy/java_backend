ef00336a579380bc3949b43c382d3e6e15a3d630
刘长玲 <changling_liu@medcn.cn>
2017-09-27 09:12:50 +0800

    service方法

--- yaya/user/src/main/java/cn/medcn/user/service/CspUserService.java
+++ yaya/user/src/main/java/cn/medcn/user/service/CspUserService.java
@@ -8,4 +8,37 @@ import cn.medcn.user.model.CspUserInfo;
  */
 public interface CspUserService extends BaseService<CspUserInfo>{
 
+    /**
+     * 根据uniqueId 查询用户是否存在
+     * @param uniqueId
+     * @return
+     */
+    CspUserInfo findBindUserByUniqueId(String uniqueId);
+
+    /**
+     * 根据邮箱或者手机号码检查csp账号 是否存在
+     * @param username
+     * @return
+     */
+    CspUserInfo findByLoginName(String username);
+
+    /**
+     * 注册用户
+     * @param userInfo
+     */
+    void register(CspUserInfo userInfo);
+
+    /**
+     * 发送手机验证码
+     * @param mobile
+     * @param type
+     */
+    void sendCaptcha(String mobile, Integer type);
+
+    /**
+     * 检查验证码是否有效
+     * @param captcha
+     * @param mobile
+     */
+    void checkCaptchaIsOrNotValid(String captcha, String mobile);
 }
