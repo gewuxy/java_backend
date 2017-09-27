eb25cc32106b9a8ebdfbddf8a39cc23df25d3874
lixuan <lixuan@medcn.cn>
2017-09-27 09:33:32 +0800

    修改MD5Utils

---
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/MeetingController.java
@@ -0,0 +1,16 @@
+package cn.medcn.csp.controller.api;
+
+import cn.medcn.common.ctrl.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lixuan on 2017/9/27.
+ */
+@Controller
+@RequestMapping(value = "/api/meeting")
+public class MeetingController extends BaseController {
+
+
+
+}
