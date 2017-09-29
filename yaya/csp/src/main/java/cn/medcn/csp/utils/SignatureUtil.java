5ee953b9d1ebc7f6a9926fb4aa3e2acd593d9956
liuliping <1047923274@qq.com>
2017-09-29 18:55:28 +0800

    update

--- yaya/csp/src/main/java/cn/medcn/csp/utils/SignatureUtil.java
+++ yaya/csp/src/main/java/cn/medcn/csp/utils/SignatureUtil.java
@@ -1,18 +1,27 @@
 package cn.medcn.csp.utils;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.*;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Enumeration;
 
 /**
- * ping++支付签名工具
+ * ping++支付签名工具,paypal支付工具
  * Created by LiuLP on 2017/9/27/027.
  */
 public class SignatureUtil {
+
+
+    private static final String TOKEN_URL = "https://api.sandbox.paypal.com/v1/oauth2/token";
+    private static final String PAYMENT_DETAIL = "https://api.sandbox.paypal.com/v1/payments/payment/";
+    private static final String clientId = "clientId";
+    private static final String secret = "secret";
 
     /**
      * 获取签名
@@ -109,6 +118,76 @@ public class SignatureUtil {
         signature.initVerify(publicKey);
         signature.update(dataString.getBytes("UTF-8"));
         return signature.verify(signatureBytes);
+    }
+
+    /**
+     * 获取token
+     * 了解更多：https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+     * @return
+     */
+    private static String getAccessToken(){
+        try{
+            URL url = new URL(TOKEN_URL);
+            String authorization = clientId+":"+secret;
+            authorization = Base64.encodeBase64String(authorization.getBytes());
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");// 提交模式
+            //设置请求头header
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Accept-Language", "en_US");
+            conn.setRequestProperty("Authorization", "Basic "+authorization);
+            // conn.setConnectTimeout(10000);//连接超时 单位毫秒
+            // conn.setReadTimeout(2000);//读取超时 单位毫秒
+            conn.setDoOutput(true);// 是否输入参数
+            String params = "grant_type=client_credentials";
+            conn.getOutputStream().write(params.getBytes());// 输入参数
+
+            InputStreamReader inStream = new InputStreamReader(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(inStream);
+            StringBuilder result = new StringBuilder();
+            String lineTxt = null;
+            while((lineTxt = reader.readLine()) != null){
+                result.append(lineTxt);
+            }
+            reader.close();
+            String accessToken = JSONObject.parseObject(result.toString()).getString("access_token");
+            return accessToken;
+        }catch(Exception err){
+            err.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取支付详情
+     * 了解更多：https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+     * @param paymentId 支付ID，来自于用户客户端
+     * @return
+     */
+    public static String getPaymentDetails(String paymentId){
+        try{
+            URL url = new URL(PAYMENT_DETAIL+paymentId);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");// 提交模式
+            //设置请求头header
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Authorization", "Bearer "+getAccessToken());
+            // conn.setConnectTimeout(10000);//连接超时 单位毫秒
+            // conn.setReadTimeout(2000);//读取超时 单位毫秒
+            InputStreamReader inStream = new InputStreamReader(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(inStream);
+            StringBuilder result = new StringBuilder();
+            String lineTxt = null;
+            while((lineTxt = reader.readLine()) != null){
+                result.append(lineTxt);
+            }
+            reader.close();
+            return result.toString();
+        }catch(Exception err){
+            err.printStackTrace();
+        }
+        return null;
     }
 
 }
