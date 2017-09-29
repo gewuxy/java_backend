18f78cc002a2ae3ea8690e47a237a8329a593ead
刘长玲 <changling_liu@medcn.cn>
2017-09-29 19:24:36 +0800

    1.在mailBean增加邮件模板枚举

--- yaya/common/src/main/java/cn/medcn/common/email/MailBean.java
+++ yaya/common/src/main/java/cn/medcn/common/email/MailBean.java
@@ -17,4 +17,36 @@ public class MailBean {
     private String subject;
     private String context;
 
+    /**
+     * 邮箱模板内容
+     */
+    public enum MailTemplate{
+        REGISTER(0, "register"), // 注册
+        FIND_PWD(1, "pwdRest"), // 找回密码
+        BIND(2, "bindEmail"); // 绑定
+
+        private Integer labelId;
+        private String label;
+
+        public Integer getLabelId() {
+            return labelId;
+        }
+
+        public void setLabelId(Integer labelId) {
+            this.labelId = labelId;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+        public void setLabel(String label) {
+            this.label = label;
+        }
+
+        MailTemplate(Integer labelId, String label){
+            this.labelId = labelId;
+            this.label = label;
+        }
+    }
 }
