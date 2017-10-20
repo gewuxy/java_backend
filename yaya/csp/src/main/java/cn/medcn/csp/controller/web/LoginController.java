fa089f79f997d3c3790446340e61ac104385b71b
刘长玲 <changling_liu@medcn.cn>
2017-10-20 17:56:58 +0800

    手机登录

--- yaya/csp/src/main/java/cn/medcn/csp/controller/web/LoginController.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/web/LoginController.java
@@ -1,17 +1,25 @@
 package cn.medcn.csp.controller.web;
 
 import cn.medcn.common.ctrl.BaseController;
+import cn.medcn.common.excptions.SystemException;
+import cn.medcn.common.utils.APIUtils;
 import cn.medcn.common.utils.CheckUtils;
 import cn.medcn.common.utils.SpringUtils;
+import cn.medcn.common.utils.StringUtils;
+import cn.medcn.user.dto.Captcha;
 import cn.medcn.user.model.BindInfo;
+import cn.medcn.user.model.CspUserInfo;
+import cn.medcn.user.service.CspUserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by lixuan on 2017/10/16.
@@ -19,6 +27,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/mgr")
 public class LoginController extends BaseController {
+
+    @Autowired
+    protected CspUserService cspUserService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Integer thirdPartyId){
@@ -31,15 +42,23 @@ public class LoginController extends BaseController {
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(String username, String password, Integer thirdPartyId, Model model){
+    public String login(String username, String password, Integer thirdPartyId,
+                        String mobile, String captcha, Model model){
         //默认为邮箱密码登录
         if (thirdPartyId == null) {
             thirdPartyId = BindInfo.Type.EMAIL.getTypeId();
         }
 
         if (thirdPartyId == BindInfo.Type.EMAIL.getTypeId()) {
+            // 邮箱登录
             return loginByEmail(username, password, model);
+
+        } else if (thirdPartyId == BindInfo.Type.MOBILE.getTypeId()) {
+            // 手机登录
+            return loginByMobile(mobile, captcha, model);
         }
+
+
         return "";
     }
 
@@ -75,4 +94,60 @@ public class LoginController extends BaseController {
         return "redirect:/mgr/meet/list";
     }
 
+    /**
+     * 手机登录
+     * @param mobile
+     * @param captcha
+     * @param model
+     * @return
+     */
+    protected String loginByMobile(String mobile, String captcha, Model model)   {
+        String errorForwardUrl = localeView("/login/login_" + BindInfo.Type.MOBILE.getTypeId());
+        if (StringUtils.isEmpty(mobile)) {
+            model.addAttribute("error", local("user.empty.mobile"));
+            model.addAttribute("mobile", mobile);
+            return errorForwardUrl;
+        }
+
+        if (StringUtils.isEmpty(captcha)) {
+            model.addAttribute("error", local("user.empty.captcha"));
+            model.addAttribute("captcha", captcha);
+            return errorForwardUrl;
+        }
+
+        try {
+            // 检查验证码是否有效
+            cspUserService.checkCaptchaIsOrNotValid(mobile, captcha);
+
+            UsernamePasswordToken token = new UsernamePasswordToken(mobile, "");
+            Subject subject = SecurityUtils.getSubject();
+            subject.login(token);
+
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("mobile", mobile);
+            return errorForwardUrl;
+        }
+        return "redirect:/mgr/meet/list";
+    }
+
+    /**
+     * 获取手机验证码
+     * @param mobile
+     * @return
+     */
+    @RequestMapping(value = "/sendCaptcha")
+    @ResponseBody
+    public String sendCaptcha(String mobile) {
+        if(!StringUtils.isMobile(mobile)){
+            return error(local("user.mobile.format"));
+        }
+
+        try {
+            return cspUserService.sendCaptcha(mobile, Captcha.Type.LOGIN.getTypeId());
+
+        } catch (SystemException e) {
+            return error(e.getMessage());
+        }
+    }
 }
