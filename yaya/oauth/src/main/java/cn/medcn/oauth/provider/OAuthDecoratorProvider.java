164e3cdda93a4f0dd923720cc48045ead0c77dbe
lixuan <lixuan@medcn.cn>
2017-10-24 15:35:29 +0800

    OAuthDecorator创建修改

--- yaya/oauth/src/main/java/cn/medcn/oauth/provider/OAuthDecoratorProvider.java
+++ yaya/oauth/src/main/java/cn/medcn/oauth/provider/OAuthDecoratorProvider.java
@@ -4,10 +4,14 @@ import cn.medcn.oauth.OAuthConstants;
 import cn.medcn.oauth.config.OAuthServiceConfig;
 import cn.medcn.oauth.decorator.OAuthServiceDecorator;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by lixuan on 2017/9/15.
  */
 public class OAuthDecoratorProvider {
+
+    private static ConcurrentHashMap<Integer, OAuthServiceDecorator> decoratorMap = new ConcurrentHashMap<>();
 
     private OAuthDecoratorProvider(){
 
@@ -30,46 +34,54 @@ public class OAuthDecoratorProvider {
 
     //todo 未完善其他三方平台创建
     public static OAuthServiceDecorator getDecorator(int serviceId, String callback){
-        OAuthServiceConfig config = new OAuthServiceConfig();
-        String appKey = null;
-        String secret = null;
-        switch (serviceId) {
-            case 1 : // 微信
-                appKey = OAuthConstants.get("WeChat.app_key");
-                secret = OAuthConstants.get("WeChat.app_secret");
-                config.setApiKey(appKey);
-                config.setApiSecret(secret);
-                config.setCallback(callback);
-                config.setScope("snsapi_userinfo");
-                config.setServiceId(serviceId);
-                break;
-            case 2 : // 微博
-                appKey = OAuthConstants.get("WeiBo.app_key");
-                secret = OAuthConstants.get("WeiBo.app_secret");
-                config.setApiKey(appKey);
-                config.setApiSecret(secret);
-                config.setCallback(callback);
-                config.setScope("");
-                config.setServiceId(serviceId);
-                break;
-            case 3 : // facebook
 
-                break;
-            case 4 : // twitter
+        if (decoratorMap.get(serviceId) != null) {
+            return decoratorMap.get(serviceId);
+        } else {
 
-                break;
-            case 5 : // YaYa医师
-                appKey = OAuthConstants.get("YaYa.app_key");
-                secret = OAuthConstants.get("YaYa.app_secret");
-                config.setApiKey(appKey);
-                config.setApiSecret(secret);
-                config.setCallback(callback);
-                config.setScope("snsapi_userinfo");
-                config.setServiceId(serviceId);
-                break;
-            default:
-                break;
+            OAuthServiceConfig config = new OAuthServiceConfig();
+            String appKey = null;
+            String secret = null;
+            switch (serviceId) {
+                case 1 : // 微信
+                    appKey = OAuthConstants.get("WeChat.app_key");
+                    secret = OAuthConstants.get("WeChat.app_secret");
+                    config.setApiKey(appKey);
+                    config.setApiSecret(secret);
+                    config.setCallback(callback);
+                    config.setScope("snsapi_userinfo");
+                    config.setServiceId(serviceId);
+                    break;
+                case 2 : // 微博
+                    appKey = OAuthConstants.get("WeiBo.app_key");
+                    secret = OAuthConstants.get("WeiBo.app_secret");
+                    config.setApiKey(appKey);
+                    config.setApiSecret(secret);
+                    config.setCallback(callback);
+                    config.setScope("");
+                    config.setServiceId(serviceId);
+                    break;
+                case 3 : // facebook
+
+                    break;
+                case 4 : // twitter
+
+                    break;
+                case 5 : // YaYa医师
+                    appKey = OAuthConstants.get("YaYa.app_key");
+                    secret = OAuthConstants.get("YaYa.app_secret");
+                    config.setApiKey(appKey);
+                    config.setApiSecret(secret);
+                    config.setCallback(callback);
+                    config.setScope("snsapi_userinfo");
+                    config.setServiceId(serviceId);
+                    break;
+                default:
+                    break;
+            }
+            OAuthServiceDecorator decorator = getDecorator(config);
+            decoratorMap.put(serviceId, decorator);
+            return decorator;
         }
-        return getDecorator(config);
     }
 }
