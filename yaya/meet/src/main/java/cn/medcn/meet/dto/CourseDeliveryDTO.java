ba6c3dba8c0e8bfa70ab1f32e7b51c177b5c778d
刘长玲 <changling_liu@medcn.cn>
2017-10-25 16:28:05 +0800

    playtime为0的情况 返回00'00“格式

--- yaya/meet/src/main/java/cn/medcn/meet/dto/CourseDeliveryDTO.java
+++ yaya/meet/src/main/java/cn/medcn/meet/dto/CourseDeliveryDTO.java
@@ -82,7 +82,7 @@ public class CourseDeliveryDTO implements Serializable {
     public String getPlayTime(){
         int pt = getDuration();
         if (pt == 0) {
-            return  " - ";
+            return  "00′00″";
         } else {
             StringBuffer buffer = new StringBuffer();
             buffer.append(pt / 60);
