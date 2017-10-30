b728b89d287192d5e0874717636820bda25e0da4
刘长玲 <changling_liu@medcn.cn>
2017-10-30 16:48:09 +0800

    增加登录成功绑定极光接口

---
+++ yaya/article/src/main/java/cn/medcn/article/service/impl/CspAppVideoServiceImpl.java
@@ -0,0 +1,29 @@
+package cn.medcn.article.service.impl;
+
+import cn.medcn.article.dao.CspAppVideoDAO;
+import cn.medcn.article.model.AppVideo;
+import cn.medcn.article.service.CspAppVideoService;
+import cn.medcn.common.service.impl.BaseServiceImpl;
+import com.github.abel533.mapper.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Liuchangling on 2017/10/30.
+ */
+@Service
+public class CspAppVideoServiceImpl extends BaseServiceImpl<AppVideo> implements CspAppVideoService {
+
+    @Autowired
+    protected CspAppVideoDAO appVideoDAO;
+
+    @Override
+    public Mapper<AppVideo> getBaseMapper() {
+        return appVideoDAO;
+    }
+
+    @Autowired
+    public AppVideo findCspAppVideo(){
+        return appVideoDAO.findCspAppVideo();
+    }
+}
