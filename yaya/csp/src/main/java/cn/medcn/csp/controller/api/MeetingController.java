299509cc34cc1a46ee3718782e8ed6106b0239f9
刘长玲 <changling_liu@medcn.cn>
2017-11-08 17:50:59 +0800

    接口复制副本，增加返回值 返回复制后的courseId

--- yaya/csp/src/main/java/cn/medcn/csp/controller/api/MeetingController.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/MeetingController.java
@@ -162,13 +162,16 @@ public class MeetingController extends CspBaseController {
         if (!principal.getId().equals(course.getCspUserId())) {
             return error(local("meeting.error.not_mine"));
         }
-        if (courseId == null || courseId == 0) {
+        if (courseId == null || courseId ==0) {
             return error(local("error.param"));
         }
         if (StringUtils.isEmpty(title)) {
             return error(local("error.param"));
         }
-        return success(audioService.addCourseCopy(courseId, title));
+        int newCourseId = audioService.addCourseCopy(courseId, title);
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", newCourseId);
+        return success(map);
     }
 
 
