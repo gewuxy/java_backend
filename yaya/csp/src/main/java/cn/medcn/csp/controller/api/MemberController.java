cbe72103d067f8740a1fa3262f9a8c4848451da1
刘长玲 <changling_liu@medcn.cn>
2017-12-12 10:25:52 +0800

    修改用户当前套餐版本接口

--- yaya/csp/src/main/java/cn/medcn/csp/controller/api/MemberController.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/MemberController.java
@@ -25,9 +25,6 @@ public class MemberController extends CspBaseController{
     @Autowired
     protected CspPackageService packageService;
 
-    @Autowired
-    protected CspUserPackageService userPackageService;
-
     // 查看用户当前套餐版本
     @RequestMapping(value = "/package")
     @ResponseBody
@@ -37,52 +34,6 @@ public class MemberController extends CspBaseController{
         return success(cspPackage);
     }
 
-    /**
-     * 套餐过期提醒
-     * @return
-     */
-    @RequestMapping(value = "/expire/remind")
-    @ResponseBody
-    public String expireRemind() {
-        String userId = SecurityUtils.get().getId();
-        // 获取前端语言
-        String localStr = LocalUtils.getLocalStr();
-
-        CspPackage cspPackage = packageService.findUserPackageById(userId);
-        int packgeId = cspPackage.getId();
-        if (cspPackage != null && cspPackage.getPackageEnd() != null
-                && packgeId > CspPackage.TypeId.STANDARD.getId()) {
-            try {
-                int diffDays = CalendarUtils.daysBetween(new Date(), cspPackage.getPackageEnd());
-                if (diffDays == 5) {
-                    // 还有5天到期时提醒
-                    return success(local("fivedays.expire.remind"));
-
-                } else if (diffDays == 0){ // 已经过期提醒
-                    // 已经过期将套餐变更为标准版
-                    CspUserPackage userPackage = userPackageService.selectByPrimaryKey(userId);
-                    userPackage.setPackageId(CspPackage.TypeId.STANDARD.getId());
-                    userPackageService.updateByPrimaryKeySelective(userPackage);
-
-                    //  已经使用的会议数 -3 = 隐藏的会议数
-                    int hiddenMeetCount = cspPackage.getLimitMeets() - 3;
-                    String remind = null;
-                    if (localStr.equals(LocalUtils.Local.en_US.name())) {
-                        remind = hiddenMeetCount+" meeting(s) hidden. Please check in website.";
-                    } else if (localStr.equals(LocalUtils.Local.zh_TW.name())) {
-                        remind = "已隱藏"+hiddenMeetCount+"個會議，請在WEB端登錄查看";
-                    } else {
-                        remind = "已隐藏"+hiddenMeetCount+"个会议，请在WEB端登录查看";
-                    }
-                    return success(remind);
-                }
-            } catch (ParseException e) {
-                e.printStackTrace();
-                return error(local("data.error"));
-            }
-        }
-        return error(local("data.error"));
-    }
 
 }
 
