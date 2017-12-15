001343de10b2419852b9732afb565d6321f0d6a3
liuliping <1047923274@qq.com>
2017-12-15 09:59:11 +0800

    update

--- yaya/csp/src/main/java/cn/medcn/csp/controller/api/ChargeController.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/ChargeController.java
@@ -93,7 +93,7 @@ public class ChargeController extends CspBaseController {
         Float money = FluxOrder.getInternalPrice(flux);
         try {
             //生成Charge对象
-            charge = chargeService.createCharge(orderNo, money, channel, ip,"流量充值");
+            charge = chargeService.createCharge(orderNo, money, channel, ip,"流量充值",appId);
         } catch (Exception e) {
             e.printStackTrace();
             return error(local("charge.fail"));
