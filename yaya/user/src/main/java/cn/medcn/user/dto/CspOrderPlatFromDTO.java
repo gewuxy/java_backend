2d1ba9f37da6b32355858e4e61497544b7d9d50c
huanghuibin <269555051@qq.com>
2017-12-22 17:10:32 +0800

    。。

---
+++ yaya/user/src/main/java/cn/medcn/user/dto/CspOrderPlatFromDTO.java
@@ -0,0 +1,34 @@
+package cn.medcn.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 各渠道数据
+ *
+ * by create HuangHuibin 2017/12/22
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CspOrderPlatFromDTO {
+
+    //注册时间
+    private Date createTime;
+
+    //web端支付宝支付
+    private float alipayWap;
+
+    //web端微信支付
+    private float wxPubQr;
+
+    //web端银联支付
+    private float upacpWap;
+
+    //总额
+    private float money;
+}
