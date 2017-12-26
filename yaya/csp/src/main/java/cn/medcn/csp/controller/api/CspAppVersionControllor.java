35bf432aac878fe4331165ff388b270aedb618fd
lixuan <lixuan@medcn.cn>
2017-12-26 09:43:33 +0800

    增加最新版本功能

--- yaya/csp/src/main/java/cn/medcn/csp/controller/api/CspAppVersionControllor.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/CspAppVersionControllor.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,6 +30,7 @@ public class CspAppVersionControllor extends CspUserController {
     protected String appFileBase;
 
     @RequestMapping(value = "/newly")
+    @ResponseBody
     public String newly(HttpServletRequest request) {
         //从header中获取版本以及系统信息
         String versionStr = request.getHeader(Constants.APP_VERSION_KEY);
