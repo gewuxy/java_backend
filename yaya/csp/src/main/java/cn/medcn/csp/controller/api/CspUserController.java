8146e09d5c24415d5a79837c5d81048924c0f22a
huanghuibin <269555051@qq.com>
2017-12-29 17:56:29 +0800

    update

--- yaya/csp/src/main/java/cn/medcn/csp/controller/api/CspUserController.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/CspUserController.java
@@ -279,12 +279,13 @@ public class CspUserController extends CspBaseController {
             if (cspUserPackage == null){
                 if(userInfoDTO.getThirdPartyId().equals(BindInfo.Type.YaYa.getTypeId())){
                     cspUserService.yayaBindUpdate(userInfo.getId());
+                }else if(userInfo.getState() == true && !userInfoDTO.getThirdPartyId().equals(BindInfo.Type.YaYa.getTypeId())){
+                    modifyOldUser(userInfo);
                 }else{
                     //app端用户默认给标准版
                     cspUserPackageService.addStanardInfo(userInfo.getId());
                 }
             }
-            modifyOldUser(userInfo);
             // 缓存用户信息
             Principal principal = cachePrincipal(userInfo);
 
