d41f1be995eac27f239c56cbbc8366ed3dcc8c7f
huanghuibin <269555051@qq.com>
2018-01-04 09:40:14 +0800

    后台定时任务配置

---
+++ yaya/csp_admin/src/main/java/cn/medcn/csp/admin/tasks/RegisterReportTask.java
@@ -0,0 +1,17 @@
+package cn.medcn.csp.admin.tasks;
+
+import cn.medcn.user.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by lixuan on 2017/12/26.
+ */
+public class RegisterReportTask {
+
+    @Autowired
+    protected ReportService reportService;
+
+    public void execute(){
+        reportService.executeReportRegister();
+    }
+}
