fc547bc9ae296b66250e9da58f76f487b6853c5b
简亮 <18379009802@163.com>
2018-01-04 13:41:17 +0800

    后台流量管理修改

--- yaya/user/src/main/java/cn/medcn/user/dao/FluxOrderDAO.java
+++ yaya/user/src/main/java/cn/medcn/user/dao/FluxOrderDAO.java
@@ -3,6 +3,7 @@ package cn.medcn.user.dao;
 import cn.medcn.user.model.FluxOrder;
 import cn.medcn.user.model.UserFlux;
 import com.github.abel533.mapper.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -18,4 +19,13 @@ public interface FluxOrderDAO extends Mapper<FluxOrder>{
      */
     List<FluxOrder> findFluxOrderList(Map<String, Object> params);
 
+    float selectAllMoney();
+
+    float selectAllMoneyByPapal();
+
+    List<FluxOrder> findFluxOrderListByUs(Map<String, Object> params);
+
+    Float findOrderListByTime(@Param("startTime") String startTime, @Param("endTime")String endTime);
+
+    Float findOrderListByTimeUs(@Param("startTime") String startTime,@Param("endTime") String endTime);
 }
