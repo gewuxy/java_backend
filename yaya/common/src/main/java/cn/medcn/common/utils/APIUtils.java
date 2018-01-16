cd82af305852b7c396a2928df8b971487db4d207
刘长玲 <changling_liu@medcn.cn>
2018-01-16 10:12:14 +0800

    1. 增加用户被冻结的错误码 2. 修改返回给前端的用户被冻结的错误码提示

--- yaya/common/src/main/java/cn/medcn/common/utils/APIUtils.java
+++ yaya/common/src/main/java/cn/medcn/common/utils/APIUtils.java
@@ -14,20 +14,26 @@ public class APIUtils {
 
     public static final String MESSAGE_KEY = "err";
 
-    public static final String ERROR_CODE = "-1";
-
-    //该用户已注册
-    public static final String USER_EXIST_CODE = "101";
-    //该用户已绑定
-    public static final String USER_BIND_CODE = "102";
-
     public static final String SUCCESS_CODE = "0" ;
+
+    public static final String ERROR_CODE = "-1";
 
     //-2代表密码错误，前端在此接口需要根据密码错误的次数弹出修改密码提示
     public static final String ERROR_PASSWORD = "-2";
 
+    //该用户已注册
+    public static final String USER_EXIST_CODE = "101";
+
+    //该用户已绑定
+    public static final String USER_BIND_CODE = "102";
+
+    // 用户未认证错误码
     public static final String ERROR_CODE_UNAUTHED = "100";
 
+    // 用户账号被冻结错误码
+    public static final String ERROR_CODE_FROZEN = "103";
+
+    // 用户象数不足错误码
     public static final String ERROR_CODE_NOTENOUGH_CREDITS = "201";
 
     // 导出excel文件出错
