4b652600699b626a365aca74d4a331053f63dd9e
刘长玲 <changling_liu@medcn.cn>
2018-01-22 17:59:00 +0800

    运营管理后台工具控制器类

--- yaya/csp_admin/src/main/java/cn/medcn/csp/admin/controllor/yaya/ShopController.java
+++ yaya/csp_admin/src/main/java/cn/medcn/csp/admin/controllor/yaya/ShopController.java
@@ -1,11 +1,14 @@
 package cn.medcn.csp.admin.controllor.yaya;
 
 import cn.medcn.common.ctrl.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Liuchangling on 2018/1/22.
  * 象城商品、订单管理控制器
  */
-
+@Controller
+@RequestMapping("/yaya/shop")
 public class ShopController extends BaseController {
 }
