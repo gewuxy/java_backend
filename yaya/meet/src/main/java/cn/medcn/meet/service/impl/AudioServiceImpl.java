3057fc2c20c09989e4e2a48f8a985ed6a6cc838b
lixuan <lixuan@medcn.cn>
2018-02-05 12:08:09 +0800

    修改已删除讲本编辑提示语

--- yaya/meet/src/main/java/cn/medcn/meet/service/impl/AudioServiceImpl.java
+++ yaya/meet/src/main/java/cn/medcn/meet/service/impl/AudioServiceImpl.java
@@ -843,7 +843,7 @@ public class AudioServiceImpl extends BaseServiceImpl<AudioCourse> implements Au
             throw new SystemException(local("source.not.exists"));
         }
         if (course.getDeleted() != null && course.getDeleted()) {
-            throw new SystemException(local("course.error.api.deleted"));
+            throw new SystemException(local("source.has.deleted"));
         }
         if (course.getPlayType() == null) {
             course.setPlayType(AudioCourse.PlayType.normal.getType());
