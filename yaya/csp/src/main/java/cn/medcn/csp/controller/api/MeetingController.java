2f68ee5ebffd09cf6e4ab9a91fadcf345a9edbd1
lixuan <lixuan@medcn.cn>
2018-02-05 15:42:16 +0800

    修改音频上传不了的BUG

--- yaya/csp/src/main/java/cn/medcn/csp/controller/api/MeetingController.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/MeetingController.java
@@ -391,7 +391,7 @@ public class MeetingController extends CspBaseController {
         //删除源文件
         FileUtils.deleteTargetFile(sourcePath);
 
-        return handleUploadResult(record, relativePath, saveFileName);
+        return handleUploadResult(record, relativePath, saveFileName + suffix);
     }
 
 
