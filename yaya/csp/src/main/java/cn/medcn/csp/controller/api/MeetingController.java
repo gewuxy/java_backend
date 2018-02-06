a9fe208fc391199dc18914fbbef7df86b76febc0
刘长玲 <changling_liu@medcn.cn>
2018-02-06 11:34:43 +0800

    1.修改小程序模板封面图片 2.修改背景音乐sql 删除where条件中的recomSort>0

--- yaya/csp/src/main/java/cn/medcn/csp/controller/api/MeetingController.java
+++ yaya/csp/src/main/java/cn/medcn/csp/controller/api/MeetingController.java
@@ -51,6 +51,7 @@ import java.util.concurrent.TimeUnit;
 
 import static cn.medcn.common.Constants.*;
 import static cn.medcn.csp.CspConstants.*;
+import static cn.medcn.meet.dto.AudioCourseDTO.HandelCoverUrl;
 
 /**
  * 会议控制器
@@ -1592,6 +1593,7 @@ public class MeetingController extends CspBaseController {
     public String templateList() {
         Principal principal = SecurityUtils.get();
         List<AudioCourseDTO> templateList = audioService.findMiniTemplate();
+        AudioCourseDTO.HandelCoverUrl(templateList, null, fileBase);
         return success(templateList);
     }
 
@@ -1611,6 +1613,7 @@ public class MeetingController extends CspBaseController {
             id = 0;
         }
         AudioCourseDTO courseDTO = audioService.findMiniTemplateByIdOrRand(id);
+        HandelCoverUrl(null, courseDTO, fileBase);
         return success(courseDTO);
     }
 
