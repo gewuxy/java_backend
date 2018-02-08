82b7f96cf076ee0df726165117e92f6c4d1c7565
刘长玲 <changling_liu@medcn.cn>
2018-02-08 09:56:14 +0800

    csp运营后台管理，会议管理增加编辑新增讲本模板

--- yaya/csp_admin/src/main/java/cn/medcn/csp/admin/controllor/CspMeetController.java
+++ yaya/csp_admin/src/main/java/cn/medcn/csp/admin/controllor/CspMeetController.java
@@ -2,9 +2,16 @@ package cn.medcn.csp.admin.controllor;
 
 import cn.medcn.common.Constants;
 import cn.medcn.common.ctrl.BaseController;
+import cn.medcn.common.ctrl.FilePath;
+import cn.medcn.common.dto.FileUploadResult;
 import cn.medcn.common.excptions.SystemException;
 import cn.medcn.common.pagination.MyPage;
 import cn.medcn.common.pagination.Pageable;
+import cn.medcn.common.service.FileUploadService;
+import cn.medcn.common.service.OfficeConvertProgress;
+import cn.medcn.common.service.OpenOfficeService;
+import cn.medcn.common.supports.FileTypeSuffix;
+import cn.medcn.common.supports.upload.FileUploadProgress;
 import cn.medcn.common.utils.*;
 import cn.medcn.csp.admin.log.Log;
 import cn.medcn.meet.dto.CourseDeliveryDTO;
@@ -31,11 +38,15 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -50,6 +61,9 @@ public class CspMeetController extends BaseController{
 
     @Value("${app.file.base}")
     protected String fileBase;
+
+    @Value("${app.file.upload.base}")
+    protected String fileUploadBase;
 
     @Autowired
     private AudioService audioService;
@@ -69,6 +83,10 @@ public class CspMeetController extends BaseController{
     @Autowired
     protected RedisCacheUtils<String> redisCacheUtils;
 
+    @Autowired
+    protected FileUploadService fileUploadService;
+    @Autowired
+    protected OpenOfficeService openOfficeService;
 
     @RequestMapping(value="/list")
     @Log(name = "获取会议列表")
@@ -86,13 +104,21 @@ public class CspMeetController extends BaseController{
         return "/meet/meetList";
     }
 
-    @RequestMapping(value = "/save")
-    @Log(name = "编辑新增讲本信息")
-    public String editMeetInfo(AudioCourse course) {
-        if (course != null){
-
-        }
+    @RequestMapping(value = "/edit")
+    @Log(name = "跳转编辑讲本页面")
+    public String toEditAudioCourse() {
         return "/meet/editMeetInfo";
+    }
+
+    @RequestMapping(value = "/save")
+    @Log(name = "编辑讲本信息")
+    public String saveAudioCourseInfo(AudioCourse course,Model model,RedirectAttributes redirectAttributes) {
+        if (course != null){
+            audioService.updateByPrimaryKey(course);
+            addFlashMessage(redirectAttributes, "编辑讲本信息成功");
+            return "redirect:/csp/meet/list";
+        }
+        return null;
     }
 
 
@@ -181,7 +207,109 @@ public class CspMeetController extends BaseController{
         return success(cspBase + "/mgr/user/cache/download?key=" + key);
     }
 
+    /**
+     * 上传PPT或者PDF文件 并转换成图片
+     *
+     * @param file
+     * @param courseId
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/upload")
+    @ResponseBody
+    public String upload(@RequestParam(value = "file") MultipartFile file, Integer courseId, HttpServletRequest request) {
+        try {
+            if (courseId != null) {
+                audioService.editAble(courseId);
+            } else {
+                // 创建讲本模板
+                AudioCourse audioCourse = audioService.createNewCspCourse(null);
+                courseId = audioCourse.getId();
+            }
+
+        } catch (SystemException e) {
+            return error(e.getMessage());
+        }
+
+        String fileName = file.getOriginalFilename();
+        FileUploadResult result;
+        try {
+            result = fileUploadService.upload(file, FilePath.TEMP.path);
+        } catch (SystemException e) {
+            return local("upload.error");
+        }
+        String imgDir = FilePath.COURSE.path + "/" + courseId + "/ppt/";
+        List<String> imgList = null;
+        if (result.getRelativePath().endsWith(FileTypeSuffix.PPT_SUFFIX_PPT.suffix) || result.getRelativePath().endsWith(FileTypeSuffix.PPT_SUFFIX_PPTX.suffix)) {
+            imgList = openOfficeService.convertPPT(fileUploadBase + result.getRelativePath(), imgDir, courseId, request);
+        } else if (result.getRelativePath().endsWith(FileTypeSuffix.PDF_SUFFIX.suffix)) {
+            imgList = openOfficeService.pdf2Images(fileUploadBase + result.getRelativePath(), imgDir, courseId, request);
+        }
+        if (CheckUtils.isEmpty(imgList)) {
+            return error(local("upload.convert.error"));
+        }
+        AudioCourse course = audioService.selectByPrimaryKey(courseId);
+        if (course != null) {
+            course.setSourceType(AudioCourse.SourceType.template.ordinal());
+            course.setTitle(fileName.substring(0, fileName.lastIndexOf(".")));
+            audioService.updateByPrimaryKey(course);
+        }
+        audioService.updateAllDetails(courseId, imgList);
+        Map<String, Object> map = new HashMap<>();
+        map.put("coverUrl", fileBase + imgList.get(0));
+        map.put("title", course.getTitle());
+        map.put("course",course);
+        return success(map);
+    }
 
 
+    @RequestMapping(value = "/upload/progress")
+    @ResponseBody
+    public String uploadProgress(HttpServletRequest request){
+        FileUploadProgress progress = (FileUploadProgress) request.getSession().getAttribute(Constants.UPLOAD_PROGRESS_KEY);
+        if(progress == null){
+            progress = new FileUploadProgress();
+        }
+        return success(progress);
+    }
 
+    @RequestMapping(value = "/upload/clear")
+    @ResponseBody
+    public String uploadClear(HttpServletRequest request){
+        request.getSession().removeAttribute(Constants.UPLOAD_PROGRESS_KEY);
+        return success();
+    }
+
+
+    @RequestMapping(value = "/convert/progress")
+    @ResponseBody
+    public String convertProgress(HttpServletRequest request){
+        uploadClear(request);
+        OfficeConvertProgress progress = (OfficeConvertProgress) request.getSession().getAttribute(Constants.OFFICE_CONVERT_PROGRESS);
+        if (progress == null) {
+            progress = new OfficeConvertProgress(0, 0, 0);
+        }
+        return success(progress);
+    }
+
+
+    @RequestMapping(value = "/convert/clear")
+    @ResponseBody
+    public String convertClear(HttpServletRequest request){
+        request.getSession().removeAttribute(Constants.OFFICE_CONVERT_PROGRESS);
+        return success();
+    }
+
+    @RequestMapping(value = "/upload/cover")
+    @ResponseBody
+    @Log(name = "上传讲本封面")
+    public String uploadPicture(@RequestParam MultipartFile file) {
+        FileUploadResult result;
+        try {
+            result = fileUploadService.upload(file, FilePath.COURSE_COVER.path);
+        } catch (SystemException e) {
+            return error(e.getMessage());
+        }
+        return success(result);
+    }
 }
